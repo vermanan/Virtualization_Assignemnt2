65fd36b18b696c401a02ade1d24dfa63312137ac
Nancy Verma <vermanancy043@gmail.com>
2024-12-10 20:58:56 -0500

    Second commit

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000; 
  
 app.get('/', (req, res) => { 
-    res.send('Hello, Dockerized Node.js App!'); 
+    res.send('Hello, Nancy Dockerized Node.js App!'); 
 }); 
  
 app.listen(PORT, () => { 
